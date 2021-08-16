a2eece9ea95159f1e1d5fe7820e13027578df724
andreikovna <d.andreikovna@gmail.com>
2021-08-17 00:43:55 +0300

    feat: functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
 
 exports.min = function min (array) {
-  return 0;
+    if (array === undefined || array.length === 0  ){
+        return 0;
+      } else {
+        return Math.min.apply(null, array);
+      }
 }
 
 exports.max = function max (array) {
-  return 0;
+    if (array === undefined || array.length === 0 ){
+        return 0;
+      } else {
+        return Math.max.apply(null, array);
+      }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array === undefined || array.length === 0  ){
+    return 0;
+  } else {
+      let i = 0;
+      let sum = 0;
+      while (i < array.length) {
+          sum += array[i];
+          i += 1;
+      }
+      let result; 
+      result = sum / array.length;
+      return result;
+  }
+    
 }
